676c6f7accbf7157cf020516a83a32014f0db88b
NathanPB <nathan.pbombana@gmail.com>
2019-05-24 02:02:39 -0300

    Added favicon

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <html>
     <head>
         <title>CoffeeMaker</title>
+        <link rel="shortcut icon" href="https://image.flaticon.com/icons/png/512/130/130484.png" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
         <link rel="stylesheet" href="assets/css/common.css">
